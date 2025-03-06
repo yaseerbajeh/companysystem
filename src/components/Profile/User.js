5a40066ef1b505038ecb61c191a1e629021939da
Abdelqader <aboodz3d@gmail.com>
2025-03-06 21:35:09 +0300

    first commit

--- src/components/Profile/User.js
+++ src/components/Profile/User.js
@@ -4,7 +4,7 @@ const User = () =>{
     return(
         <div className="flex gap-3 items-center bg-white p-4
         rounded-full dark:bg-gray-600 dark:text-gray-300">
-            <img src={userLogo} alt="user image" className="w-14 h-14
+            <img src={userLogo} alt="\" className="w-14 h-14
             rounded-full" />
             <div>
                 <h3 className="font-semibold text-2xl">Jhon Doe</h3>
